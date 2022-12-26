9be274b316b1b7b5fc4c40598f7c1894207df7e1
Demich <hdcv564@gmail.com>
2022-12-26 12:50:28 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/zoomapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zoomapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:zoomapp/screens/login_screen.dart';
+import 'package:zoomapp/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ZOOM',
+      theme: ThemeData.dark().copyWith(
+        
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      routes:{
+        '/login':(context) => const LoginScreen(),
+      },
+      home: const LoginScreen()
+    );
+  }
+}
+
