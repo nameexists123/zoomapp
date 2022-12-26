1125a1c5e2987217d79349f0262d7f970c9f6892
Demich <hdcv564@gmail.com>
2022-12-26 16:30:50 -0500

    autenticacion de google

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoomapp/screens/home_screen.dart';
 import 'package:zoomapp/screens/login_screen.dart';
 import 'package:zoomapp/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,66 +7,74 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _page = 0;
-  onPageChanged(int page) {
-    setState(() {
-      _page = page;
-    });
+class _HomeScreenState extends State<HomeScreen>{
+  @override 
+  Widget build(BuildContext context){
+    return Container();
   }
-
-  List<Widget> pages = [
-    MeetingScreen(),
-    const HistoryMeetingScreen(),
-    const Text('Contacts'),
-    CustomButton(text: 'Log Out', onPressed: () => AuthMethods().signOut()),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: backgroundColor,
-        elevation: 0,
-        title: const Text('Meet & Chat'),
-        centerTitle: true,
-      ),
-      body: pages[_page],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: footerColor,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        onTap: onPageChanged,
-        currentIndex: _page,
-        type: BottomNavigationBarType.fixed,
-        unselectedFontSize: 14,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.comment_bank,
-            ),
-            label: 'Meet & Char',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.lock_clock,
-            ),
-            label: 'Meetings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_outline,
-            ),
-            label: 'Contacts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.settings_outlined,
-            ),
-            label: 'Settings',
-          ),
-        ],
-      ),
-    );
-  }
+  
 }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   int _page = 0;
+//   onPageChanged(int page) {
+//     setState(() {
+//       _page = page;
+//     });
+//   }
+
+  // List<Widget> pages = [
+  //   MeetingScreen(),
+  //   const HistoryMeetingScreen(),
+  //   const Text('Contacts'),
+  //   CustomButton(text: 'Log Out', onPressed: () => AuthMethods().signOut()),
+  // ];
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: backgroundColor,
+  //       elevation: 0,
+  //       title: const Text('Meet & Chat'),
+  //       centerTitle: true,
+  //     ),
+  //     body: pages[_page],
+  //     bottomNavigationBar: BottomNavigationBar(
+  //       backgroundColor: footerColor,
+  //       selectedItemColor: Colors.white,
+  //       unselectedItemColor: Colors.grey,
+  //       onTap: onPageChanged,
+  //       currentIndex: _page,
+  //       type: BottomNavigationBarType.fixed,
+  //       unselectedFontSize: 14,
+  //       items: const [
+  //         BottomNavigationBarItem(
+  //           icon: Icon(
+  //             Icons.comment_bank,
+  //           ),
+  //           label: 'Meet & Char',
+  //         ),
+  //         BottomNavigationBarItem(
+  //           icon: Icon(
+  //             Icons.lock_clock,
+  //           ),
+  //           label: 'Meetings',
+  //         ),
+  //         BottomNavigationBarItem(
+  //           icon: Icon(
+  //             Icons.person_outline,
+  //           ),
+  //           label: 'Contacts',
+  //         ),
+  //         BottomNavigationBarItem(
+  //           icon: Icon(
+  //             Icons.settings_outlined,
+  //           ),
+  //           label: 'Settings',
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
